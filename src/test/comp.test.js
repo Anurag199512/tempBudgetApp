5745d37366e8c4cbb5c78dbc5ad08849dbc3dd57
Anurag199512 <anurag199512@gmail.com>
2019-12-10 20:43:09 +0530

    saving the styled app

--- src/test/comp.test.js
+++ src/test/comp.test.js
@@ -1,25 +1,3 @@
-const mul=(a,b)=>{
-    return a*b
-}
+import {ReactShallowRenderer} from 'react-test-renderer/shallow'
+import {Approute} from '../component/maincomp'
 
-test('addd 2 no',()=>{
-    const result=mul(3,4)
-
-    //1 way // if(result!== 7)
-    //     {throw new Error('Tc failed...')}
-
-    //2 way
-    expect(result).toBe(12)
-})
-
-
-
-test('addd 2 no',()=>{
-    const result=mul(3,4)
-
-    //1 way // if(result!== 7)
-    //     {throw new Error('Tc failed...')}
-
-    //2 way
-    expect(result).toBe(12)
-})
